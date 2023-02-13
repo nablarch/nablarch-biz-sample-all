af746660ac5d6b3412bfa45644423c9e1ca26478
sukegawatakashitis <sukegawa.takashi@tis.co.jp>
2023-02-13 14:37:00 +0900

    feat: ファイルを論理削除するSQLを削除しました。

--- src/main/resources/please/change/me/common/file/management/entity/FileControl.sql
+++ src/main/resources/please/change/me/common/file/management/entity/FileControl.sql
@@ -11,15 +11,3 @@ FROM
 WHERE
     FILE_CONTROL_ID = ?
     AND SAKUJO_SGN='0'
-
-
---------------------------------------------------------------------------------
--- ファイルを論理削除するSQL
---------------------------------------------------------------------------------
-DELETE_FILE_CONTROL = 
-UPDATE FILE_CONTROL 
-SET
-    SAKUJO_SGN='1'
-WHERE
-    FILE_CONTROL_ID = ?
-    AND SAKUJO_SGN='0'
