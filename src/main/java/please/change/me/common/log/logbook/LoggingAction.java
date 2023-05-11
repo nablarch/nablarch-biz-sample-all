3da46e550e0e675f5513fc13a0d0644e5f4684d2
hijiri <3445928+hiji@users.noreply.github.com>
2023-05-12 08:49:07 +0900

    Logbookの実装例をJava8でコンパイルできるように修正 (#24)

--- src/main/java/please/change/me/common/log/logbook/LoggingAction.java
+++ src/main/java/please/change/me/common/log/logbook/LoggingAction.java
@@ -47,7 +47,7 @@ public class LoggingAction {
 
         // レスポンスをオブジェクトに変換
         String json = response.readEntity(String.class);
-        List<User> responseBody = new ObjectMapper().readValue(json, new TypeReference<>() {});
+        List<User> responseBody = new ObjectMapper().readValue(json, new TypeReference<List<User>>() {});
 
         return responseBody;
     }
@@ -80,7 +80,7 @@ public class LoggingAction {
 
         // レスポンスをオブジェクトに変換
         String json = response.readEntity(String.class);
-        List<User> responseBody = new ObjectMapper().readValue(json, new TypeReference<>() {});
+        List<User> responseBody = new ObjectMapper().readValue(json, new TypeReference<List<User>>() {});
 
         return responseBody;
     }
